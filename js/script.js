8c49638e1a9ff0ece14335032fdeb2d8bd0d28b9
malikal ibni sultana <125950776+KalCuy@users.noreply.github.com>
2023-03-20 12:02:05 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//toggle
+const NavbarNav = document.querySelector
+('.navbar-nav');
+document.querySelector('#hamburger-menu').onclick = () => {
+    NavbarNav.classList.toggle('active');
+};
+
+//sidebar untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click' , function (e) {
+    if (!hamburger.contains(e.target) && !NavbarNav.contains(e.target)) {
+        NavbarNav.classList.remove('active');
+    }
+});
